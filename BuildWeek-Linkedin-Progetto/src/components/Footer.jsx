bb5aa56852ea97513c2ea89e23be6fc2ca1484e5
AndreaCeliberti <andrea.celiberti2@gmail.com>
2025-10-21 14:40:55 +0200

    footer font fixed

--- BuildWeek-Linkedin-Progetto/src/components/Footer.jsx
+++ BuildWeek-Linkedin-Progetto/src/components/Footer.jsx
@@ -8,47 +8,47 @@ const Footer = () => {
       <Container className="text-secondary">
         <Row className="mt-5">
           <Col xl={2} className="d-flex flex-column align-items-start gap-2 ">
-            <button className="foot-font-size text-secondary fw-bold foot-btn">Informazioni</button>
-            <button className="foot-font-size text-secondary fw-bold foot-btn text-start">Informativa sulla community professionale</button>
-            <button className="foot-font-size text-secondary fw-bold foot-btn">
+            <button className="nav-font-size text-secondary fw-bold foot-btn">Informazioni</button>
+            <button className="nav-font-size text-secondary fw-bold foot-btn text-start">Informativa sulla community professionale</button>
+            <button className="nav-font-size text-secondary fw-bold foot-btn">
               Privacy e condizioni
-              <BiSolidDownArrow className="ms-1 foot-font-size text-secondary" />{" "}
+              <BiSolidDownArrow className="ms-1 nav-font-size text-secondary" />{" "}
             </button>
-            <button className="foot-font-size text-secondary fw-bold foot-btn">Sales Solutions</button>
-            <button className="foot-font-size text-secondary fw-bold foot-btn">Centro sicurezza</button>
+            <button className="nav-font-size text-secondary fw-bold foot-btn">Sales Solutions</button>
+            <button className="nav-font-size text-secondary fw-bold foot-btn">Centro sicurezza</button>
           </Col>
           <Col xl={2} className="d-flex flex-column align-items-start gap-2 ">
-            <button className="foot-font-size text-secondary fw-bold foot-btn">Accessibilità</button>
-            <button className="foot-font-size text-secondary fw-bold foot-btn">Carriera</button>
-            <button className="foot-font-size text-secondary fw-bold foot-btn text-start">Opzioni per gli anunci pubblicitari </button>
-            <button className="foot-font-size text-secondary fw-bold foot-btn">Mobile</button>
+            <button className="nav-font-size text-secondary fw-bold foot-btn">Accessibilità</button>
+            <button className="nav-font-size text-secondary fw-bold foot-btn">Carriera</button>
+            <button className="nav-font-size text-secondary fw-bold foot-btn text-start">Opzioni per gli anunci pubblicitari </button>
+            <button className="nav-font-size text-secondary fw-bold foot-btn">Mobile</button>
           </Col>
           <Col xl={2} className="d-flex flex-column align-items-start gap-2 ">
-            <button className="foot-font-size text-secondary fw-bold foot-btn">Talent solutions</button>
-            <button className="foot-font-size text-secondary fw-bold foot-btn">Soluzioni di marketing</button>
-            <button className="foot-font-size text-secondary fw-bold foot-btn">Pubblicità </button>
-            <button className="foot-font-size text-secondary fw-bold foot-btn">Piccole imprese</button>
+            <button className="nav-font-size text-secondary fw-bold foot-btn">Talent solutions</button>
+            <button className="nav-font-size text-secondary fw-bold foot-btn">Soluzioni di marketing</button>
+            <button className="nav-font-size text-secondary fw-bold foot-btn">Pubblicità </button>
+            <button className="nav-font-size text-secondary fw-bold foot-btn">Piccole imprese</button>
           </Col>
           {/* footer icons */}
           <Col xl={3} className="d-flex flex-column align-items-start gap-2 ">
             <div className="d-flex gap-2 align-items-start">
               <AiFillQuestionCircle className="fs-4" />
               <div className="d-flex align-items-start flex-column gap-1">
-                <button className="nav-font-size foot-btn text-secondary fw-bold p-0">Domande ?</button>
+                <button className="foot-font-size foot-btn text-secondary fw-bold p-0">Domande ?</button>
                 <span className="foot-font-size text-secondary">Visita il nostro Centro assistenza.</span>
               </div>
             </div>
             <div className="d-flex gap-2 align-items-start">
               <IoSettingsSharp className="fs-4" />
               <div className="d-flex align-items-start flex-column gap-1">
-                <button className="nav-font-size foot-btn text-secondary fw-bold p-0">Gestisci il tuo account e la tua priacy</button>
+                <button className="foot-font-size foot-btn text-secondary fw-bold p-0">Gestisci il tuo account e la tua priacy</button>
                 <span className="foot-font-size text-secondary">Vai alle impostazioni</span>
               </div>
             </div>
             <div className="d-flex gap-2 align-items-start">
               <IoShieldHalf className="fs-4" />
               <div className="d-flex align-items-start flex-column gap-1">
-                <button className="nav-font-size foot-btn text-secondary fw-bold p-0">Trasparenza sui contenuti consigliati</button>
+                <button className="foot-font-size foot-btn text-secondary fw-bold p-0">Trasparenza sui contenuti consigliati</button>
                 <span className="foot-font-size text-secondary">Scopri di più sui contenuti consigliati.</span>
               </div>
             </div>
@@ -63,14 +63,14 @@ const Footer = () => {
                   className="bg-white rounded-2 text-secondary nav-font-size text-start language"
                   style={{ width: "282px", height: "32px" }}
                 >
-                  Seleziona lingua{" "}
+                  Sgegli lingua{""}
                 </Dropdown.Toggle>
 
                 <Dropdown.Menu>
-                  <Dropdown.Item className="nav-font-size text-secondary" href="#/action-1" active>
+                  <Dropdown.Item className="nav-font-size text-secondary" href="#/action-1">
                     Italiano
                   </Dropdown.Item>
-                  <Dropdown.Item className="nav-font-size text-secondary" href="#/action-1" active>
+                  <Dropdown.Item className="nav-font-size text-secondary" href="#/action-1">
                     Inglese
                   </Dropdown.Item>
                   <Dropdown.Item className="nav-font-size text-secondary" href="#/action-2">
@@ -107,7 +107,7 @@ const Footer = () => {
       <Container>
         <Row className="mt-3 ">
           <Col>
-            <p className="nav-font-size text-secondary">All Rights Reserved &copy; 2025</p>
+            <p className="nav-font-size text-secondary">LinkedIn Corporation &copy; 2025</p>
           </Col>
         </Row>
       </Container>
